7dfe8afc5ec64e80e2c6705ccdb2b8ca6e3b297a
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2026-02-24 17:06:00 +0100

    init

--- admin/backup.sh
+++ admin/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 $PWD/admin/externalBackup.sh iris-dev
-$PWD/admin/externalBackup.sh iris-prod-1
-$PWD/admin/externalBackup.sh iris-prod-2
+# $PWD/admin/externalBackup.sh iris-prod-1
+# $PWD/admin/externalBackup.sh iris-prod-2
