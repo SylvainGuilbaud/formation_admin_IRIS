ce97c596fea93fdb2da41c22308f50ff58c3a15a
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2026-02-27 18:31:37 +0100

    init

---
+++ docker/README.md
@@ -0,0 +1,9 @@
+> **⚠️ Prerequisites**
+> 
+> Before running `docker-compose up -d`, you **must** first execute the `perm.sh` script to set the required file permissions:
+> 
+> ```bash
+> ./perm.sh
+> ```
+> 
+> This step is mandatory to ensure that the necessary permissions are correctly applied before starting the Docker containers.
--- docker/perm.sh
+++ docker/perm.sh
@@ -1,2 +1,5 @@
+#!/bin/sh
+# This script is used to set the correct permissions on the persistent_data volume 
+# in order to allow the IRIS container to read and write to it without issues.
 docker run --rm -v docker_persistent_data:/persistent_data alpine sh -c \
 "chown -R 51773:51773 /persistent_data && chmod -R u+rwX,g+rwX /persistent_data"
